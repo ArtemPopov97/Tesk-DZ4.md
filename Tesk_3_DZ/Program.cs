9e7ae043cee0a0c9baccce0c641413c96e4d49ac
unknown <artem.popov.97@mail.ru>
2022-06-21 10:58:11 +0300

    tesk 3 array

---
+++ Tesk_3_DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу , которая задает массив 
+// из 8 элементов и выходит их на экран.
+int [] array = new int[8];
+
+void PrintArray(int [] array)
+{
+    int lenght = array.Length;
+    for(int i = 0; i< lenght; i++)
+    {
+        array[1] = new Random().Next(0, 199);
+        Console.Write($"{array[1]} ");
+    }
+}
+
+PrintArray(array);
