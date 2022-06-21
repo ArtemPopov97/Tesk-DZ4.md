867903c41ca9abb35ab88d19d849d3fa8a777ec4
unknown <artem.popov.97@mail.ru>
2022-06-21 09:45:02 +0300

    tesk 1 nature

---
+++ Tesk_1_DZ/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл,который принимает на вход два
+//  числа (А и В) и возводит число А в натуралбную степень В.
+
+Console.WriteLine("Введите число 1"); 
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 1"); 
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+int Nature (int A, int B)
+{
+    int resuilt = A;
+    for(int i = 1; i < B; i++)
+    {
+        resuilt = resuilt * A;
+    }
+    return resuilt;
+}
+
+Console.WriteLine(Nature(A,B));
