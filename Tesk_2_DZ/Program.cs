339958fef04b72e8ceeeb3996ce22e78253e7e0a
unknown <artem.popov.97@mail.ru>
2022-06-21 10:35:29 +0300

    tesk 2 summa

---
+++ Tesk_2_DZ/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу , которая принимает на    452 > 11
+// вход число и выдает сумму цифр в числе.       82 > 10
+//                                             9012 > 12
+Console.WriteLine("Введите число ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+int Summa (int N)
+{
+    int res = 0;
+    for(int i = 0; N > 0; i++)
+    {
+        int S = N % 10;
+        res = res + S;
+        N =N / 10;
+    }
+    return res;
+}
+
+Console.WriteLine(Summa(Namber));
